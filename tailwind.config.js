6f159ed099860ebcc894a2d0b32d03d30fe96adc
Rich Kanson <richardkanson@gmail.com>
2020-06-23 19:21:27 -0400

    purge whitelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  purge: {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    options: {
+      whitelist: [
+        "col-span-1",
+        "col-span-2",
+        "col-span-3",
+        "col-span-4",
+        "col-span-5",
+        "col-span-6",
+      ],
+    },
+  },
   theme: {
     extend: {
       spacing: {
