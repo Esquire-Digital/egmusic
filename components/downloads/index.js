996b674268522eebcd9881c12ab75a3f9210715b
Rich Kanson <richardkanson@gmail.com>
2020-06-23 19:29:20 -0400

    wip

--- components/downloads/index.js
+++ components/downloads/index.js
@@ -3,11 +3,11 @@ import DLImage from "./image";
 export default function Downloads() {
   return (
     <section className="relative grid max-w-6xl grid-cols-1 gap-4 p-4 mx-auto lg:gap-8 lg:p-8 lg:grid-cols-8">
-      <DLImage span="2" src="/images/dl-1.png?webp" alt="" />
-      <DLImage span="6" src="/images/dl-2.png?webp" alt="" />
-      <DLImage span="3" src="/images/dl-3.png?webp" alt="" />
-      <DLImage span="5" src="/images/dl-4.png?webp" alt="" />
-      <DLImage span="8" src="/images/dl-5.png?webp" alt="" />
+      <DLImage span="lg:col-span-2" src="/images/dl-1.png?webp" alt="" />
+      <DLImage span="lg:col-span-6" src="/images/dl-2.png?webp" alt="" />
+      <DLImage span="lg:col-span-3" src="/images/dl-3.png?webp" alt="" />
+      <DLImage span="lg:col-span-5" src="/images/dl-4.png?webp" alt="" />
+      <DLImage span="lg:col-span-8" src="/images/dl-5.png?webp" alt="" />
     </section>
   );
 }
