bfe1e094034367a5a17c4f039181c8764ff20e30
Rich Kanson <rich@nextlevel.com>
2020-07-08 14:05:27 -0400

    move

--- components/bio/index.js
+++ components/bio/index.js
@@ -23,7 +23,7 @@ export default function Bio() {
         <p className="mb-12 text-4xl font-bold leading-none lg:whitespace-no-wrap font-mont text-eg-blue">
           {title}
         </p>
-        <BioContent className="flex flex-col space-y-6 leading-loose" />
+        <BioContent />
       </div>
     </section>
   );
