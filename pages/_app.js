d9a7cb2cfb4855562cd6bfc666dd46a8a5819e18
Rich Kanson <rich@nextlevel.com>
2020-07-22 11:42:00 -0400

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,6 @@
 import "../styles.css";
-import { useEffect, useState } from 'react'
-import netlifyAuth from '../utils/netlifyAuth'
 
 function App({ Component, pageProps }) {
-  let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated)
-
-  useEffect(() => {
-    netlifyAuth.authenticate((user) => {
-      setLoggedIn(!!user)
-    })
-  }, [loggedIn])
-
   return (
     <>
       <Component {...pageProps} />
