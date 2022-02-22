ce6390c6b6155cc55ce7bda7a8c925ccd115292d
Ryan Hartman <rhartman1239@gmail.com>
2022-02-22 14:45:46 -0500

    Fix spotify embed

--- components/music-press/index.js
+++ components/music-press/index.js
@@ -1,5 +1,5 @@
 import Press from "./press";
-import { attributes } from '../../content/music-press.md';
+import { attributes } from "../../content/music-press.md";
 
 export default function MusicPress() {
   let { spotifyEmbed, press } = attributes;
@@ -9,15 +9,10 @@ export default function MusicPress() {
         <p className="text-2xl font-bold text-white uppercase font-mont">
           Music
         </p>
-        <iframe
-          src={spotifyEmbed}
-          width="300"
-          height="80"
-          frameborder="0"
-          allowtransparency="true"
-          allow="encrypted-media"
-          className="mt-8"
-        ></iframe>
+        <div
+          className="w-full mt-8"
+          dangerouslySetInnerHTML={{ __html: spotifyEmbed }}
+        />
       </div>
       <div className="flex flex-col items-center p-12 lg:p-24 lg:w-1/2 lg:items-start bg-eg-epk-2">
         <p className="text-2xl font-bold uppercase font-mont text-eg-blue">
@@ -25,11 +20,7 @@ export default function MusicPress() {
         </p>
         <ul className="flex flex-col my-8 space-y-4">
           {press.slice(0, 6).map((item, k) => (
-            <Press
-              key={k}
-              text={item.text}
-              link={item.url}
-            />
+            <Press key={k} text={item.text} link={item.url} />
           ))}
         </ul>
       </div>
